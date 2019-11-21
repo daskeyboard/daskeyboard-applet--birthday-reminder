031e30dc7f078ba948e84de2b7b9ca6abdbf8624
camillebqt <camille.bouquet@cpe.fr>
2019-11-21 10:21:09 -0600

    Test with fake date added

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+const q = require('daskeyboard-applet');
 var assert = require('assert');
 const t = require('../index');
 
@@ -13,38 +14,54 @@ describe('BirthdayReminder', () => {
         });
     });
     describe('#run()', () => {
-        it('sends a reminder ', async function () {
-            let app = await buildApp();
-            return buildApp().then(app =>{
-                return app.run().then((signal) => {
-                    assert.ok(signal);
-                    assert(signal.message.includes(defaultConfig.applet.user.nameOfTheBirthdayPerson));
-                }).catch((error) => {
-                    assert.fail(error)
-                });
-            })
+        it('sends a signal with the name of the birthday person in the signal message when it is the birthday date', async function () {
+            const config = getConfigForDate(new Date());// create a configuration: the birthday date is the current date
+            console.log('>>>>>>config:', config);
+            let app = await buildApp(config);
+            return app.run().then((signal) => {
+                assert.ok(signal);
+                assert(signal.message.includes(config.applet.user.nameOfTheBirthdayPerson));// on verifie si on recoit le signal avec le nom dedans
+                assert.equal(signal.points[0][0].effect, q.Effects.BLINK);
+            }).catch((error) => {
+                assert.fail(error)
+            });
+        });
+    });
+
+    describe('#run()', () => {
+        it('sends not a signal when it is not the birthday date', async function () {
+            const simulatedDate = getConfigForDate(new Date(2020, 11, 21));
+            console.log('>>>>>>simulatedDate', simulatedDate);
+            let app = await buildApp(simulatedDate);
+            return app.run().then((signal) => {
+                assert.equal(signal, null);
+            }).catch((error) => {
+                assert.fail(error)
+            });
         });
     });
 })
 
-const defaultConfig = {
-    applet: {
-        user: {
-            nameOfTheBirthdayPerson: 'oooo',
-            monthOfTheBirthday: 10,
-            dayOfTheBirthday: 20
+function getConfigForDate(date) {
+    const defaultConfig = {
+        applet: {
+            user: {
+                nameOfTheBirthdayPerson: 'oooo',
+                monthOfTheBirthday: date.getMonth(),
+                dayOfTheBirthday: date.getDate()
+            }
+        },
+        geometry: {
+            width: 1,
+            height: 1,
         }
-    },
-    geometry: {
-        width: 1,
-        height: 1,
-    }
-};
-
+    };
+    return defaultConfig;
+}
 
 async function buildApp(config) {
     let app = new t.BirthdayReminder();
-    await app.processConfig(config || defaultConfig);
+    await app.processConfig(config);
     return app;
 
 }
